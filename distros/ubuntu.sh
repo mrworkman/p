faffd7927878e53b52bd58ba5e18f900e364d450
Stephen Workman <workman.stephen@gmail.com>
2022-09-10 21:57:22 +0100

    Remove xrdp and python 2 for ubuntu installs

--- distros/ubuntu.sh
+++ distros/ubuntu.sh
@@ -1,15 +1,4 @@
 
-function install_pip27 {
-   [[ -e /usr/local/bin/pip2 ]] && return
-
-   # NB: WSL2 does not currently support IPv6, so curl must explicitly use `-4`.
-
-   printf "Installing pip for python2..."
-   o=$(sudo curl -s4 https://bootstrap.pypa.io/pip/2.7/get-pip.py | sudo python2 2>&1 > /dev/null)
-   r=$?
-   print_result_or_die "$o" $r
-}
-
 function install_pip3 {
    [[ -e /usr/local/bin/pip3 ]] && return
 
@@ -38,8 +27,6 @@ if [[ ! $wsl ]]; then
    install_package 'openssh-server' true
    install_package 'firefox'        true
    install_package 'gtk-chtheme'    true
-   install_package 'xrdp'           true
-   install_package 'xorgxrdp'       true
 else
    install_package 'coreutils'      true
    install_package 'zsh'            true
@@ -50,20 +37,7 @@ else
    install_package 'openssh-server' false
    install_package 'firefox'        false
    install_package 'gtk-chtheme'    false
-   install_package 'xrdp'           false
-   install_package 'xorgxrdp'       false
 fi
-
-case "$distro_release" in
-    18*)
-        install_package 'python'            true
-        install_package 'python-pip'        true
-        ;;
-    20*)
-        install_package 'python-is-python2' true
-        install_pip27
-        ;;
-esac
 
 install_package 'python3'           true
 install_package 'python3-venv'      true
