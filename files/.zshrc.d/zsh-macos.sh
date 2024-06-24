0e7f0a3f8cd7baa4d69e390a6c69131744591f14
Stephen Workman <workman.stephen@gmail.com>
2024-06-24 14:32:39 +0100

    Minor updates.

--- files/.zshrc.d/zsh-macos.sh
+++ files/.zshrc.d/zsh-macos.sh
@@ -10,24 +10,24 @@ zstyle ':completion:*' accept-exact '*(N)'
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path ~/.zsh/cache
 
-# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md
-source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 # https://github.com/zsh-users/zsh-history-substring-search
 source /opt/homebrew/share/zsh-history-substring-search/zsh-history-substring-search.zsh
 
 # https://github.com/zsh-users/zsh-autosuggestions#configuration
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters.md
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
 zmodload zsh/terminfo
 bindkey "$terminfo[kcuu1]" history-substring-search-up
 bindkey "$terminfo[kcud1]" history-substring-search-down
-bindkey '^[[A' history-substring-search-up			
+bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=253"
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND="bg=220,fg=7"
-HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND="bg=1,fg=0"
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND="bg=220,fg=0"
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND="bg=red,fg=white,bold"
 HISTORY_SUBSTRING_SEARCH_FUZZY=on
 HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=on
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
